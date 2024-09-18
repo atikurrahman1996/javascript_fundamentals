914ef1a27b4c1f488814b2783fa382fc73a1dce7
Atikur Rahman <atikur2013rahman@gmail.com>
2024-09-18 10:21:27 +0600

    ternary operator

---
+++ day2_Conditionals.js
@@ -0,0 +1,17 @@
+//Ternary Operator
+
+let age = 18;
+
+let type = age >= 15 ? "adult" : "child";
+
+console.log(type);
+
+var a = 6;
+var access = a > 5;
+console.log(access);
+
+//Syntex: variablename = (condition) ? value1:value2
+
+let age2 = 10;
+let voteable = age2 < 18 ? "Too young" : "Old enough";
+console.log(voteable);
